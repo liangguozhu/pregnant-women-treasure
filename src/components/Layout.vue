249997da12ebcf327b54581bba2697cec3ecaa2e
lllgz <424348921@qq.com>
2023-03-17 19:16:52 +0800

    专家

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -5,6 +5,10 @@
   </el-header>
   <el-main>
     <router-view></router-view>
+
+    <div class="ask">
+      联系专家
+    </div>
   </el-main>
 </el-container>
 </template>
@@ -30,5 +34,19 @@ export default {
   height: calc(100vh - 60px);
   overflow-y: auto;
   overflow-x: hidden;
+}
+
+.ask {
+  cursor: pointer;
+  font-size: 16px;
+  background: #fff;
+  padding: 20px 10px;
+  width: 20px;
+  text-align: center;
+  position: fixed;
+  font-weight: bold;
+  right: 0;
+  bottom: 100px;
+  box-shadow: 0 0 2px 3px #ddd;
 }
 </style>
