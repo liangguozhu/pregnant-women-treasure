b37bd8adc14eaf800507b21d5be43b31252e374c
lllgz <424348921@qq.com>
2023-03-20 18:40:58 +0800

    专家

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -6,8 +6,27 @@
   <el-main>
     <router-view></router-view>
 
-    <div class="ask">
-      联系专家
+    <div class="chat" :class="showChatBox ? 'show' : 'hide'">
+      <div class="ask" @click="contact">
+        联系专家
+      </div>
+      <div class="chat-box">
+        <div class="chat-header">
+          专家-王女士
+          <i class="el-icon-close" style="float: right; cursor: pointer;" @click="contact"></i>
+        </div>
+
+        <div class="chat-body">
+          <div class="message-content">
+            <msg v-for="i in msgs" :key="i.key" :dir="i.dir" :content="i.content"></msg>
+          </div>
+          <div class="message-box">
+            <el-input placeholder="您可以直接输入问题向专家提问" v-model="msg">
+              <template slot="append"><span @click="ask" style="cursor:pointer;">发送</span></template>
+            </el-input>
+          </div>
+        </div>
+      </div>
     </div>
   </el-main>
 </el-container>
@@ -15,10 +34,31 @@
 
 <script>
 import Header from "@/components/Header";
+import Msg from "@/components/common/msg";
 
 export default {
   components: {
-    Header
+    Header,
+    Msg
+  },
+  data() {
+    return {
+      showChatBox: false,
+      msg: '',
+      msgs: [
+        { key: 0, dir: true, content: 'Hi，请问有什么可以帮您~' }
+      ]
+    }
+  },
+  methods: {
+    contact() {
+      this.showChatBox = !this.showChatBox
+    },
+    ask() {
+      this.msgs.push({ key: this.msgs.length, dir: false, content: this.msg });
+      this.msg = '';
+      console.log('触发')
+    }
   }
 }
 </script>
@@ -36,17 +76,78 @@ export default {
   overflow-x: hidden;
 }
 
+.chat {
+  display: flex;
+  align-items: flex-end;
+  position: fixed;
+  right: -500px;
+  bottom: 50px;
+}
+
 .ask {
+  margin-bottom: 40px;
   cursor: pointer;
   font-size: 16px;
   background: #fff;
   padding: 20px 10px;
   width: 20px;
   text-align: center;
-  position: fixed;
   font-weight: bold;
-  right: 0;
-  bottom: 100px;
   box-shadow: 0 0 2px 3px #ddd;
+  border-radius: 3px;
+}
+
+.chat-box {
+  width: 500px;
+  height: 500px;
+  background: #fff;
+}
+
+.chat-header {
+  box-sizing: border-box;
+  padding: 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+  box-shadow: 0 1px 1px 1px #eee;
+}
+
+.chat-body {
+  box-sizing: border-box;
+  padding: 10px;
+  height: 449px;
+  display: flex;
+  flex-flow: column nowrap;
+}
+
+.message-content {
+  flex: 1;
+  overflow-x: hidden;
+  overflow-y: auto;
+}
+
+.message-content::-webkit-scrollbar {
+  display: none;
+}
+
+.message-box {
+  flex: none;
+  width: 100%;
+}
+
+@keyframes chat-box-display {
+  from { right: -500px; }
+  to { right: 0; }
+}
+@keyframes chat-box-hide {
+  from { right: 0; }
+  to { right: -500px; }
+}
+.show {
+  animation: chat-box-display 200ms linear forwards;
+}
+.hide {
+  animation: chat-box-hide 200ms linear forwards;
 }
 </style>
---
+++ src/components/common/msg.vue
@@ -0,0 +1,51 @@
+<template>
+<div>
+  <div class="msg">
+    <el-avatar :class="dir ? 'f-l' : 'f-r'" src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"></el-avatar>
+    <div :class="dir ? 'f-l' : 'f-r'" class="content">{{ content }}</div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "msg",
+  props: ['dir', 'content'],
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.f-l {
+  float: left;
+}
+.f-r {
+  float: right;
+  background: #6ff12f!important;
+}
+
+.msg {
+  overflow: hidden;
+  margin-bottom: 20px;
+}
+.el-avatar {
+  display: inline-block;
+  width: 30px;
+  height: 30px;
+}
+.content {
+  display: inline-block;
+  border-radius: 5px;
+  box-sizing: border-box;
+  padding: 0 10px;
+  background: #f1f3f0;
+  margin: 0 10px;
+  max-width: calc(100% - 50px);
+  line-height: 30px;
+  box-shadow: 1px 1px 1px 1px rgba(0,0,0,.1);
+}
+</style>
